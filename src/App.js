3c962481a6fea488865ace24978a045628b9bf93
NataWhite <nata.ivanova.one@gmail.com>
2021-05-06 02:53:15 +0300

    JSON posts

--- src/App.js
+++ src/App.js
@@ -3,10 +3,18 @@
 //     За рендер окремого поста відповідатиме компонент Post . В компоненті поста повинна бути кнопка, при кліку
 // на яку поруч зі списком всіх постів з'являється обраний пост і його деталі.
 
+import Users from "./components/users/Users";
+import Posts from "./components/posts/Posts";
+
 function App() {
     return (
         <div>
-
+            <div>
+                <Users/>
+            </div>
+            <div>
+                <Posts/>
+            </div>
         </div>
     );
 }
