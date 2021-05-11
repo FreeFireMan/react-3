6183196c9bce2b5e1b33f9ed055346b35543c3dd
NataWhite <nata.ivanova.one@gmail.com>
2021-05-11 16:51:55 +0300

    futurama api

--- src/App.js
+++ src/App.js
@@ -1,36 +1,17 @@
-import Users from "./components/users/Users";
-import Posts from "./components/posts/Posts";
-import Comments from "./components/comments/Comments";
-import {BrowserRouter as Router, Link, Route, Switch} from "react-router-dom";
-import User from "./components/user/User";
-import Description from "./components/Description/Description";
-
+// #routing #route #api
+// Беремо цю апі -https://sampleapis.com/api-list/futurama.
+// Головна сторінка - 2 роути на персонажів та інвентар.
+//     При кліку на відповідний роут - перехід на сторінку зі
+// списком персонажів/інвентаря (коротка інформація)
+// При кліку на конкрентний елемент - перехід на сторінку  цього елементу з детальною інформацією.
+//     З урахуванням того, що апі не надає можливості отримати поодинокі об'єкит, ' +
+// 'продумайте,як ви будете діставати їх на окремій сторінці з деталями.
 
 function App() {
     return (
-        <Router>
-            <div>
-                <Link to={'users'}>open users</Link>
-            </div>
-            <div>
-                <Link to={'posts'}>open posts</Link>
-            </div>
-            <div>
-                <Link to={'comments'}>open comments</Link>
-            </div>
+       <div>
 
-            <Switch>
-                <Route path={'/users'} render={(props) => <Users {...props}/>}/>
-                <Route path={'/posts'} render={() => <Posts/>}/>
-                <Route path={'/comments'} render={() => <Comments/>}/>
-
-                <Route exact={true} path={'/users/:id'}
-                       render={(props) => {
-                           let {match: {param: {id}}} = props
-                           return <Description userId={id}/>
-                       }}/>
-            </Switch>
-        </Router>
+       </div>
 
 
     );
