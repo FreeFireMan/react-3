13ef798775a911dba578ee6536097419e6660eef
NataWhite <nata.ivanova.one@gmail.com>
2021-05-13 13:38:18 +0300

    api reqres

--- src/App.js
+++ src/App.js
@@ -1,42 +1,20 @@
-// #route #routing
-// Головна сторінка має лінки на сторінки з юзерами та постами з jsonplaceholer.
-//     При кліку на лінку юзерс відбувається перехід на сторінку зі всіма юзерами.
-//     В кожного юзера є кнопка-лінка, при кліку на яку відображаються всі тільки його пости.
-//     Та сама ситуація з постами, тільки при кліку на окремий пост відображається коментарі цього поста
+// #routing #route #api
+// Беремо цю апі - https://reqres.in/
+// з неї використовувати тільки get запити на list users та single user.
+//     На головній сторінці є роут який одразу відображає  користувачів.
+//     (Зауважте, що апі не віддає всіх користувачів,а має queryParametr/serach з назвою page ,
+//     який характеризує пагінацію, та віддає по 10 об'єктів за один запит).
+// Внизу є 2 кнопки при натисканні на які ви ідете до наступної/попередньої сторінки (змінюючи page параметр).
+// при клыку на користувача відбувається перехід на сторінку з детальною інформацією користувача
 
 import {BrowserRouter as Router, Link, Route, Switch} from "react-router-dom";
-import Users from "./component/users/Users";
-import UserPosts from "./component/user.posts/UserPosts";
-import Posts from "./component/posts/Posts";
-import Comments from "./component/comments/Comments";
 
 
 function App() {
     return (
-        <Router>
-            <div>
-                <Link to={'/'}>Home</Link>
-            </div>
-            <div>
-                <Link to={'/users'}>Users</Link>
-            </div>
-            <div>
-                <Link to={'/posts'}>Posts</Link>
-            </div>
+       <div>
 
-            <Switch>
-                <Route exact={true} path={'/'}/>
-                <Route exact={true} path={'/users'}
-                       render={(props) => <Users {...props}/>}/>
-                <Route path={'/users/:id'}
-                       render={(props) => <UserPosts {...props}/>}/>
-                <Route exact={true} path={'/posts'}
-                       render={(props) => <Posts {...props}/>}/>
-                <Route path={'/posts/:id'}
-                       render={(props) => <Comments {...props}/>}/>
-
-            </Switch>
-        </Router>
+       </div>
     );
 };
 
