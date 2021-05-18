d6b0fd69e505a006f0acc8ce8a73848c619e3c3f
NataWhite <nata.ivanova.one@gmail.com>
2021-05-18 15:07:08 +0300

    counter

--- src/App.js
+++ src/App.js
@@ -1,27 +1,13 @@
-// #routing #route #api
-// Беремо цю апі - https://reqres.in/
-// з неї використовувати тільки get запити на list users та single user.
-//     На головній сторінці є роут який одразу відображає  користувачів.
-//     (Зауважте, що апі не віддає всіх користувачів,а має queryParametr/serach з назвою page ,
-//     який характеризує пагінацію, та віддає по 10 об'єктів за один запит).
-// Внизу є 2 кнопки при натисканні на які ви ідете до наступної/попередньої сторінки (змінюючи page параметр).
-// при клыку на користувача відбувається перехід на сторінку з детальною інформацією користувача
+// Зробити компоненту Counter, в якій будуть кнопки +1, -1, +100, -100.
+// Де Counter це окрема компонента, на яку ми попадаємо через роути. Функція reducer винесена в окремий файл!
 
 import {BrowserRouter as Router, Link, Route, Switch} from "react-router-dom";
-import Users from "./components/users/Users";
-import Details from "./components/details/Details";
 
 function App() {
 
     return (
         <Router>
-            <Link to={{pathname: '/users', search:'?page=1'}}>Show users</Link>
-            <Switch>
-                <Route  exact={true} path={'/users'}
-                       render={(props) => <Users {...props}/>}/>
-                <Route exact={true} path={'/users/:id'}
-                       render={(props)=><Details {...props}/>}/>
-            </Switch>
+
         </Router>
     );
 }
