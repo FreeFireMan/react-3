0a92e5fbb7dc14029fb175ac5858cc5444ac3f5d
freefireman <beluh.c.s@gmail.com>
2021-05-19 17:12:27 +0300

    add action-types

---
+++ src/redux/action-types/counter-action-types.js
@@ -0,0 +1,4 @@
+export const INCREMENT = 'INCREMENT';
+export const DECREMENT = 'DECREMENT';
+export const PLUS_HUNDRED = 'PLUS_HUNDRED';
+export const MINUS_HUNDRED = 'MINUS_HUNDRED';
--- src/redux/reducers/counter-reducer.js
+++ src/redux/reducers/counter-reducer.js
@@ -1,22 +1,24 @@
+import {INCREMENT, DECREMENT, PLUS_HUNDRED, MINUS_HUNDRED} from '../action-types';
+
 const initialState = {
     counter: 0
 }
 
 export const counterReducer = (state=initialState, action) => {
     switch (action.type) {
-        case 'INCREMENT':
+        case INCREMENT:
             return {
                 ...state, counter: state.counter + 1
             }
-        case 'DECREMENT':
+        case DECREMENT:
             return {
                 ...state, counter: state.counter - 1
             }
-        case 'PLUS_HUNDRED':
+        case PLUS_HUNDRED:
             return {
                 ...state, counter: state.counter + 100
             }
-        case 'MINUS_HUNDRED' :
+        case MINUS_HUNDRED:
             return {
                 ...state, counter: state.counter - 100
             }
