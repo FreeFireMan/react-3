4ff03d557a074c70ed490ea673a1eb37608cc263
NataWhite <nata.ivanova.one@gmail.com>
2021-05-19 18:50:41 +0300

    react-redux

--- src/App.js
+++ src/App.js
@@ -1,18 +1,13 @@
-// Зробити компоненту Counter, в якій будуть кнопки +1, -1, +100, -100.
-// Де Counter це окрема компонента, на яку ми попадаємо через роути. Функція reducer винесена в окремий файл!
 
 import './App.css'
-import {BrowserRouter as Router, Route, Link} from "react-router-dom";
-import Counter from "./components/counter/Counter";
+
 
 function App() {
 
     return (
-        <Router>
-            <Link to={'/counter'}>Open counter</Link>
+        <div>
 
-            <Route path={'/counter'} render={()=><Counter/>}/>
-        </Router>
+        </div>
     );
 }
 
